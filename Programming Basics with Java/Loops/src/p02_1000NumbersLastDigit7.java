3ea89f55b5a95bb4da11c7d2cfafeb70e80f7650
The 3D <the3ddd@live.com>
2017-09-29 08:00:48 +0300

    Adding the rest of the homeworks 29/09/2017

--- Programming Basics with Java/Loops/src/p02_1000NumbersLastDigit7.java
+++
@@ -1,9 +0,0 @@
-public class p02_1000NumbersLastDigit7 {
-    public static void main(String[] args) {
-        for(int i = 0 ; i <= 1000; i++ ){
-            if(i % 10 == 7){
-                System.out.println(i);
-            }
-        }
-    }
-}
--- Programming Basics with Java/Loops/src/p03_alfabetPrint.java
+++
@@ -1,7 +0,0 @@
-public class p03_alfabetPrint {
-    public static void main(String[] args) {
-        for(char letter = 'a'; letter <= 'z';letter++){
-            System.out.println(letter);
-        }
-    }
-}
